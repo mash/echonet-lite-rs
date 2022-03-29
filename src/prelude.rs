52895fcdd0bd2c034e806064c3a6d38c202c055b
Masakazu Ohtsuka <o.masakazu@gmail.com>
2022-03-30 08:29:09 +0900

    These localized names are useful

--- src/prelude.rs
+++ src/prelude.rs
@@ -2,3 +2,6 @@
 
 pub use crate::el_packet::*;
 pub use crate::object::ClassPacket;
+pub use crate::object::{
+    EVPS_CLASS, HOUSEHOLD_SOLAR_POWER_CLASS, PROFILE_CLASS, STORAGE_BATTERY_CLASS, SUPER_CLASS,
+};
